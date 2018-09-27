67be628afda580fbe34374585a4cc427cfd5d1f1
Jé <38817318+JessicaCarmona@users.noreply.github.com>
2018-09-27 10:11:37 -0400

    Update media.py

--- media.py
+++ media.py
@@ -4,7 +4,7 @@ class Movie():
     """This class stores movies information"""
     
     def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube): 
-    #This part of the code is to store data for each movie:
+    #This part of the code is to store the data:
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image 
