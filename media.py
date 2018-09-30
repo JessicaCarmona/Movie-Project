e8fbcd07cbe775a43553f80ffb3dcfa8ac36a82e
Jé <38817318+JessicaCarmona@users.noreply.github.com>
2018-09-30 12:49:35 -0400

    Update media.py

--- media.py
+++ media.py
@@ -1,14 +1,23 @@
 import webbrowser
 
+
 class Movie():
-    """This class stores movies information"""
-    
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube): 
-    #This part of the code is to store the data:
-        self.title = movie_title
-        self.storyline = movie_storyline
-        self.poster_image_url = poster_image 
-        self.trailer_youtube_url = trailer_youtube
-    #This code will execute the trailer:
+    """This class stores movies information
+
+      Atributes:
+          title (str): Title of the movie
+          storyline (str): Short Storyline of the movie
+          poster_image (str): Link to the Poster Image of the movie
+          trailer_url(str): Link to Trailer of the movie (e.g. youtube url)
+    """
+
+    def __init__(self, title, storyline, poster_image, trailer_url):
+        # This part of the code is to store data for each movie:
+        self.title = title
+        self.storyline = storyline
+        self.poster_image_url = poster_image
+        self.trailer_youtube_url = trailer_url
+
     def show_trailer(self):
+        # This code will execute the trailer:
         webbrowser.open(self.trailer_youtube_url)
